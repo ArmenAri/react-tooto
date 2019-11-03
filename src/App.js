db8c10417f095c5909bfd8ed8e9f137a9d7ff64a
Armen Aristakesyan <armenaristakes@gmail.com>
2019-11-03 21:35:37 +0100

    :sparkles: Adding cards change system

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import "./App.css"
-import Card from "./components/Card"
-import * as dataService from "./services/dataService"
+import Screen from './components/Screen'
 
 class App extends React.Component {
-
   render() {
-    this.a = dataService.pickRandomData()
-    this.b = dataService.pickRandomData()
     return (
-      <div className="center">
-        <div className="App">
-          <header className="App-header">
-            <Card type="" title={this.a.title} desc={this.a.description} extra={this.a.extra} />
-            <Card type="secret" title={this.b.title} desc={this.b.description} extra={this.b.extra} />
-          </header>
-        </div>
-      </div>
+      <Screen />
     );
   }
 }
---
+++ src/components/Screen.js
@@ -0,0 +1,66 @@
+import React from "react"
+import Card from "./Card"
+import "./Screen.css"
+import { pickRandomData } from "../services/dataService"
+
+class Screen extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            normalCards: [
+                pickRandomData()
+            ],
+            secretCards: [
+                pickRandomData()
+            ]
+        }
+
+        this.changeCards = this.changeCards.bind(this)
+    }
+
+    changeCards = () => {
+        this.setState(
+            {
+                normalCards: [
+                    pickRandomData()
+                ],
+                secretCards: [
+                    pickRandomData()
+                ]
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className="center">
+                <div className="App">
+                    <header className="App-header">
+                        <div>
+                            {this.state.normalCards.map(card => {
+                                return <Card key={card.id}
+                                    type=""
+                                    title={card.title}
+                                    desc={card.description}
+                                    extra={card.extra} />
+                            })}
+                        </div>
+                        <div>
+                            {this.state.secretCards.map(card => {
+                                return <Card key={card.id}
+                                    type="secret"
+                                    title={card.title}
+                                    desc={card.description}
+                                    extra={card.extra} />
+                            })}
+                        </div>
+                    </header>
+                </div>
+                <button className="button" onClick={this.changeCards}>→</button>
+            </div>
+        )
+    }
+}
+
+export default Screen
